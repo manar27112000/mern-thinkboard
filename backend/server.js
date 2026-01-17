be4bfd62f5a12fb9bf15593ebb08f33a0e5b4123
manar27112000 <adel0115225@gmail.com>
2026-01-18 01:50:39 +0200

    refactor code  -add routes folder and noteRoutes file

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,8 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 const app =express();
 
-app.get("/api/notes",(req,res)=>{
-    res.status(200).send("you got 40 notes")
-});
-
-app.post("/api/notes",(req,res)=>{
-    res.status(201).json({
-        message:"note created successfully"
-    });
-});
-
-app.put("/api/notes",(req,res)=>{
-    res.status(200).json({
-        message:"note updated successfully"
-    });
-});
-
-app.delete("/api/notes/:id",(req,res)=>{
-    res.status(200).json({
-        message:"note deleted successfully"
-    });
-});
+app.use("/api/notes",notesRoutes)
 
 app.listen(5001,()=>{
     console.log("server started on port :5001")
