9b9809c230fee1eec160b2a0f0dbb4d2da32f785
manar27112000 <adel0115225@gmail.com>
2026-01-18 01:23:43 +0200

    add (get, post, put, delete) methods

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,25 @@ import express from "express";
 const app =express();
 
 app.get("/api/notes",(req,res)=>{
-    res.send("you got 5 notes")
+    res.status(200).send("you got 40 notes")
+});
+
+app.post("/api/notes",(req,res)=>{
+    res.status(201).json({
+        message:"note created successfully"
+    });
+});
+
+app.put("/api/notes",(req,res)=>{
+    res.status(200).json({
+        message:"note updated successfully"
+    });
+});
+
+app.delete("/api/notes/:id",(req,res)=>{
+    res.status(200).json({
+        message:"note deleted successfully"
+    });
 });
 
 app.listen(5001,()=>{
